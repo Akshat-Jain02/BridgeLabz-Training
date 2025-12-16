486aeee71bcbd44a73cfb9f79ed0902186b1efa9
Akshat Jain <akshatjain2102004@gmail.com>
2025-12-16 22:32:13 +0530

    [Akshat Jain]: added to calculate area of a circle

---
+++ gcr-codebase/program-prerequisites/AreaOfCircle.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class AreaOfCircle {
+   public static void main(String[]args) {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter the radius of circle");
+      double radius = sc.nextDouble();
+      double area = Math.PI * Math.pow(radius , 2);
+      System.out.println("Area of a circle is : " + area);   
+   }
+}
