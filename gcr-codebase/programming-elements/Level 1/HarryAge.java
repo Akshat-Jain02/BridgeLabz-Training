9f012263dc8be3b408e6d42be8ba64625550b3fa
Akshat Jain <akshatjain2102004@gmail.com>
2025-12-17 17:21:30 +0530

    [Akshat Jain]: added for age of Harry

---
+++ gcr-codebase/programming-elements/Level 1/HarryAge.java
@@ -0,0 +1,19 @@
+//Create HarryAge class to find the age of Harry
+
+class HarryAge {
+    public static void main(String[]args) {
+        
+        //create a variable to indicate birth year of Harry
+        int birthYear = 2000;
+
+        //create a variable currentYear to indicate currentYear
+        int currentYear = 2024;
+        
+        //create a variable age to store the age of Harry
+        int age = currentYear - birthYear;
+
+        //Print the current age
+        System.out.println("Harrys's age in 2024 is " + age);
+        
+    }
+}
