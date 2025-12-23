209bd99f25583f63be656df1f8d91646d787f806
Akshat Jain <akshatjain2102004@gmail.com>
2025-12-23 20:34:01 +0530

    [Akshat Jain]: added palindrome text program

---
+++ gcr-codebase/Strings/Level 3/PalindromeText.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+
+public class PalindromeText {
+
+    public static boolean isPalindromeLoop(String text) {
+        int start = 0;
+        int end = text.length() - 1;
+
+        while (start < end) {
+            if (text.charAt(start) != text.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    //Logic 2 using recursion
+    public static boolean isPalindromeRecursive(String text, int start, int end) {
+
+        if (start >= end) {
+            return true;
+        }
+
+        if (text.charAt(start) != text.charAt(end)) {
+            return false;
+        }
+
+        return isPalindromeRecursive(text, start + 1, end - 1);
+    }
+
+
+    // Method to reverse string using charAt()
+    public static char[] reverseString(String text) {
+        char[] reverse = new char[text.length()];
+        int index = 0;
+
+        for (int i = text.length() - 1; i >= 0; i--) {
+            reverse[index] = text.charAt(i);
+            index++;
+        }
+        return reverse;
+    }
+
+    public static boolean isPalindromeArray(String text) {
+        char[] original = text.toCharArray();
+        char[] reverse = reverseString(text);
+
+        for (int i = 0; i < original.length; i++) {
+            if (original[i] != reverse[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter text: ");
+        String text = sc.nextLine();
+
+        boolean result1 = isPalindromeLoop(text);
+        boolean result2 = isPalindromeRecursive(text, 0, text.length() - 1);
+        boolean result3 = isPalindromeArray(text);
+
+        System.out.println("\nPalindrome Check Results:");
+        System.out.println("-------------------------");
+        System.out.println("Using Loop       : " + result1 +
+                           "\n Using Recursion  : " + result2 +
+                            "\n Using Char Array : " + result3);
+
+        sc.close();
+    }
+}
