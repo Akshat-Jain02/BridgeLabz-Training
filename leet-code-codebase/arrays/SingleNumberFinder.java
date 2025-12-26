eef097b80fce0d0fd50818c031310eb52641cc78
Akshat Jain <akshatjain2102004@gmail.com>
2025-12-26 14:45:32 +0530

    [Akshat Jain]: Added single number finder program

---
+++ leet-code-codebase/arrays/SingleNumberFinder.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class SingleNumberFinder {
+
+    public static int singleNumber(int[] nums) {
+        int res = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            res = res ^ nums[i];
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input array size
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        // Input array elements
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        // Call static method
+        int result = singleNumber(nums);
+
+        // Output result
+        System.out.println("Single number is: " + result);
+
+        sc.close();
+    }
+}
