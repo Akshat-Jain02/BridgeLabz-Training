5ca8366e19855b84f5b6f37efc5d291689ea828a
Akshat Jain <akshatjain2102004@gmail.com>
2025-12-28 16:32:14 +0530

    [Akshat Jain]: Added set matrix zeroes program

---
+++ leet-code-codebase/arrays/SetMatrixZeroesApp.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class SetMatrixZeroesApp {
+
+    //Method to set matrix zero
+    public static void setZeroes(int[][] matrix) {
+        boolean[] row = new boolean[matrix.length];
+        boolean[] col = new boolean[matrix[0].length];
+
+        // Mark rows and columns that contain 0
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = true;
+                    col[j] = true;
+                }
+            }
+        }
+
+        // Set elements to 0
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (row[i] || col[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+  
+        //taking input from user
+        System.out.print("Enter number of rows: ");
+        int r = sc.nextInt();
+
+        System.out.print("Enter number of columns: ");
+        int c = sc.nextInt();
+
+        int[][] matrix = new int[r][c];
+
+        System.out.println("Enter matrix elements:");
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        setZeroes(matrix);
+
+        System.out.println("Matrix after setting zeroes:");
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
