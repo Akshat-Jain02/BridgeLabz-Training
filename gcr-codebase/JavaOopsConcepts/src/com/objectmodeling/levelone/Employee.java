258120dda5563781b9f5115a7b2af8bb490dcfc0
Akshat Jain <akshatjain2102004@gmail.com>
2025-12-31 11:45:03 +0530

    [Akshat Jain]: Added Employee.java file

---
+++ gcr-codebase/JavaOopsConcepts/src/com/objectmodeling/levelone/Employee.java
@@ -0,0 +1,11 @@
+package com.objectmodeling.levelone;
+
+public class Employee {
+
+    String employeeName;
+
+    public Employee(String employeeName) {
+        this.employeeName = employeeName;
+    }
+    
+}
