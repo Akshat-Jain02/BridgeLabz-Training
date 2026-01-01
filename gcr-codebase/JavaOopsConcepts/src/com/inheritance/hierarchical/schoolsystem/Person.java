74cf92e932c5da7c1da7d849534d0380fbe0a29b
Akshat Jain <akshatjain2102004@gmail.com>
2026-01-01 12:37:43 +0530

    [Akshat Jain]: Added Person.java file

---
+++ gcr-codebase/JavaOopsConcepts/src/com/inheritance/hierarchical/schoolsystem/Person.java
@@ -0,0 +1,27 @@
+package com.inheritance.hierarchical.schoolsystem;
+
+public class Person {
+	protected String name;
+    protected int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+
+    // To be overridden by subclasses
+    public void displayRole() {
+        System.out.println("Role: General Person in School");
+    }
+
+    public void showDetails() {
+        displayInfo();
+        displayRole();
+        System.out.println("---");
+    }
+}
