b75b9ba04ccccb15024c46699c2138fdb6c6be41
Akshat Jain <akshatjain2102004@gmail.com>
2026-01-01 13:03:37 +0530

    [Akshat Jain]: Added OnlineCourse.java file

---
+++ gcr-codebase/JavaOopsConcepts/src/com/inheritance/multilevel/educationalcourse/OnlineCourse.java
@@ -0,0 +1,21 @@
+package com.inheritance.multilevel.educationalcourse;
+
+public class OnlineCourse extends Course{
+	protected String platform;
+    protected boolean isRecorded;
+
+    public OnlineCourse(String courseName, int duration, String platform, boolean isRecorded) {
+        super(courseName, duration);
+        this.platform = platform;
+        this.isRecorded = isRecorded;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Platform: " + platform);
+        System.out.println("Recorded: " + (isRecorded ? "Yes" : "No"));
+        System.out.println("Type: Online Course");
+        System.out.println("---");
+    }
+}
