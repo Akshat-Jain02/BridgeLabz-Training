c4d49c7c794117548dbc302b46d8f863d2574a09
Akshat Jain <akshatjain2102004@gmail.com>
2026-01-01 12:58:47 +0530

    [Akshat Jain]: Added Vehicle.java file

---
+++ gcr-codebase/JavaOopsConcepts/src/com/inheritance/hybrid/vehiclemanagement/Vehicle.java
@@ -0,0 +1,21 @@
+package com.inheritance.hybrid.vehiclemanagement;
+
+public class Vehicle {
+	protected String model;
+    protected int maxSpeed;
+
+    public Vehicle(String model, int maxSpeed) {
+        this.model = model;
+        this.maxSpeed = maxSpeed;
+    }
+
+    public void displayInfo() {
+        System.out.println("Model: " + model);
+        System.out.println("Max Speed: " + maxSpeed + " km/h");
+    }
+
+    public void showDetails() {
+        displayInfo();
+        System.out.println("---");
+    }
+}
