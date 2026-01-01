894edea8a7940ba02f0460163f7c60af2a77086e
Akshat Jain <akshatjain2102004@gmail.com>
2026-01-01 12:40:13 +0530

    [Akshat Jain]: Added Student.java file

---
+++ gcr-codebase/JavaOopsConcepts/src/com/inheritance/hierarchical/schoolsystem/Student.java
@@ -0,0 +1,19 @@
+package com.inheritance.hierarchical.schoolsystem;
+
+public class Student extends Person{
+	private String grade;
+    private String studentId;
+
+    public Student(String name, int age, String grade, String studentId) {
+        super(name, age);
+        this.grade = grade;
+        this.studentId = studentId;
+    }
+
+    @Override
+    public void displayRole() {
+        System.out.println("Role: Student");
+        System.out.println("Grade: " + grade);
+        System.out.println("Student ID: " + studentId);
+    }
+}
