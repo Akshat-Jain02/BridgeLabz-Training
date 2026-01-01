187132cab392a61c8b8fb7c9c79ab8cc255fd928
Akshat Jain <akshatjain2102004@gmail.com>
2026-01-01 13:10:58 +0530

    [Akshat Jain]: Added Author.java file

---
+++ gcr-codebase/JavaOopsConcepts/src/com/inheritance/single/librarymanagement/Author.java
@@ -0,0 +1,21 @@
+package com.inheritance.single.librarymanagement;
+
+public class Author extends Book{
+	private String name;
+    private String bio;
+
+    public Author(String title, int publicationYear, String name, String bio) {
+        super(title, publicationYear);
+        this.name = name;
+        this.bio = bio;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();  // Display book details first
+        System.out.println("Author Name: " + name);
+        System.out.println("Biography: " + bio);
+        System.out.println("Type: Book with Author Details");
+        System.out.println("------------------------");
+    }
+}
