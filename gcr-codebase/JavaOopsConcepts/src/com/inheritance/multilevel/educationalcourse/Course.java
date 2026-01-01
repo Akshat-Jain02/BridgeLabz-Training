c9e99dd46331f3286ac2de9d167f744ff14013e0
Akshat Jain <akshatjain2102004@gmail.com>
2026-01-01 13:01:42 +0530

    [Akshat Jain]: Added Course.java file

---
+++ gcr-codebase/JavaOopsConcepts/src/com/inheritance/multilevel/educationalcourse/Course.java
@@ -0,0 +1,18 @@
+package com.inheritance.multilevel.educationalcourse;
+
+public class Course {
+	protected String courseName;
+    protected int duration;  // in weeks
+
+    public Course(String courseName, int duration) {
+        this.courseName = courseName;
+        this.duration = duration;
+    }
+
+    public void displayInfo() {
+        System.out.println("Course Name: " + courseName);
+        System.out.println("Duration: " + duration + " weeks");
+        System.out.println("Type: General Course");
+        System.out.println("---");
+    }
+}
