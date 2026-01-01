f51f3fc1ee7b80ef9a244f00d16f612191a0b0d1
Akshat Jain <akshatjain2102004@gmail.com>
2026-01-01 12:26:01 +0530

    [Akshat Jain]: Added Cat.java file

---
+++ gcr-codebase/JavaOopsConcepts/src/com/inheritance/hierarchical/animalhierarchy/Cat.java
@@ -0,0 +1,12 @@
+package com.inheritance.hierarchical.animalhierarchy;
+
+public class Cat extends Animal {
+	public Cat(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(getName() + " says: Meow!");
+    }
+}
