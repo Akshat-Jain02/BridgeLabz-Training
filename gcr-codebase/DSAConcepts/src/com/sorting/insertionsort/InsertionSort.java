4c27f5ee11b2051dd0f1f6b99e5fabfbc7ae8a78
Akshat Jain <akshatjain2102004@gmail.com>
2026-01-06 12:17:42 +0530

    [Akshat Jain]: Added Insertion Sort java file

---
+++ gcr-codebase/DSAConcepts/src/com/sorting/insertionsort/InsertionSort.java
@@ -0,0 +1,22 @@
+package com.sorting.insertionsort;
+
+class InsertionSort {
+
+    public void sort(int[] empIds) {
+        int n = empIds.length;
+
+        for (int i = 1; i < n; i++) {
+            int key = empIds[i];
+            int j = i - 1;
+
+            // Move elements greater than key to one position ahead
+            while (j >= 0 && empIds[j] > key) {
+                empIds[j + 1] = empIds[j];
+                j--;
+            }
+
+            // Insert key at correct position
+            empIds[j + 1] = key;
+        }
+    }
+}
