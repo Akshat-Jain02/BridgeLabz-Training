729db1bf12e90c20bc7f835e213e78991ad750d7
Akshat Jain <akshatjain2102004@gmail.com>
2026-01-23 13:10:48 +0530

    [Akshat Jain]: Added DivisionWithFinally.java file

---
+++ gcr-codebase/JavaCollectionsPractice/src/com/exceptionhandling/finallyblockexecution/DivisionWithFinally.java
@@ -0,0 +1,25 @@
+package com.exceptionhandling.finallyblockexecution;
+
+import java.util.Scanner;
+
+public class DivisionWithFinally {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter numerator: ");
+        int num = sc.nextInt();
+
+        System.out.print("Enter denominator: ");
+        int den = sc.nextInt();
+
+        try {
+            int result = num / den;
+            System.out.println("Result: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+        } finally {
+            System.out.println("Operation completed");
+            sc.close();
+        }
+    }
+}
