fb441456fcff7599887ed5afbb604dbdece04fe9
Akshat Jain <akshatjain2102004@gmail.com>
2026-01-23 13:10:05 +0530

    [Akshat Jain]: Added ExceptionPropogation.java file

---
+++ gcr-codebase/JavaCollectionsPractice/src/com/exceptionhandling/exceptionpropogation/ExceptionPropagation.java
@@ -0,0 +1,19 @@
+package com.exceptionhandling.exceptionpropogation;
+public class ExceptionPropagation {
+
+    public static void method1() {
+        int result = 10 / 0; // This will throw ArithmeticException
+    }
+
+    public static void method2() {
+        method1(); // Exception propagates from here
+    }
+
+    public static void main(String[] args) {
+        try {
+            method2();
+        } catch (ArithmeticException e) {
+            System.out.println("Handled exception in main");
+        }
+    }
+}
