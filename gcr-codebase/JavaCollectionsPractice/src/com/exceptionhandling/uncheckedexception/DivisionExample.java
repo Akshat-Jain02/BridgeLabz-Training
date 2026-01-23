b29d693e6c972b6db3f12173f5441b78da170580
Akshat Jain <akshatjain2102004@gmail.com>
2026-01-23 13:14:18 +0530

    [Akshat Jain]: Added DivisionExample.java file

---
+++ gcr-codebase/JavaCollectionsPractice/src/com/exceptionhandling/uncheckedexception/DivisionExample.java
@@ -0,0 +1,30 @@
+package com.exceptionhandling.uncheckedexception;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class DivisionExample {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter numerator: ");
+            int num = sc.nextInt();
+
+            System.out.print("Enter denominator: ");
+            int den = sc.nextInt();
+
+            int result = num / den;
+            System.out.println("Result: " + result);
+
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input, please enter numbers");
+
+        } finally {
+            sc.close();
+        }
+    }
+}
