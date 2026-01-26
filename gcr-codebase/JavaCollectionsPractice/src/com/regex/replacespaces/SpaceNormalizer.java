7912ecc89ead44daabdb15ead17ba0da6f567578
Akshat Jain <akshatjain2102004@gmail.com>
2026-01-26 21:48:15 +0530

    [Akshat Jain]: Added java program to replace multiple spaces with a single space

---
+++ gcr-codebase/JavaCollectionsPractice/src/com/regex/replacespaces/SpaceNormalizer.java
@@ -0,0 +1,13 @@
+package com.regex.replacespaces;
+
+public class SpaceNormalizer {
+
+    public static void main(String[] args) {
+
+        String input = "This   is    an   example    with   multiple   spaces.";
+
+        String output = input.replaceAll("\\s+", " ");
+
+        System.out.println(output);
+    }
+}
