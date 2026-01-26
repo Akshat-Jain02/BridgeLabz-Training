c7a3278a0f3335b28f23dbc22514e96d8cd94a6b
Akshat Jain <akshatjain2102004@gmail.com>
2026-01-26 21:44:10 +0530

    [Akshat Jain]: Added java program to extract email addresses from a sentence

---
+++ gcr-codebase/JavaCollectionsPractice/src/com/regex/extractemailaddresses/EmailExtractorApp.java
@@ -0,0 +1,22 @@
+package com.regex.extractemailaddresses;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EmailExtractorApp {
+
+    public static void main(String[] args) {
+
+        String text = "Contact us at support@example.com and info@company.org";
+
+        String regex = "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}";
+
+        Pattern pattern = Pattern.compile(regex);
+
+        Matcher matcher = pattern.matcher(text);
+
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+        }
+    }
+}
