89a081216539cb5fe8ff3a21e24b12faa4099bac
Akshat Jain <akshatjain2102004@gmail.com>
2026-02-07 08:28:04 +0530

    [Akshat Jain]: Added java files for school student system problem

---
+++ scenario-based/ScenarioPracticeJava8/src/com/lamdaexpressions/schoolstudentsystem/PassFailCheck.java
@@ -0,0 +1,6 @@
+package com.lamdaexpressions.schoolstudentsystem;
+
+@FunctionalInterface
+public interface PassFailCheck {
+    boolean check(int marks);
+}
---
+++ scenario-based/ScenarioPracticeJava8/src/com/lamdaexpressions/schoolstudentsystem/WelcomeMessage.java
@@ -0,0 +1,6 @@
+package com.lamdaexpressions.schoolstudentsystem;
+
+@FunctionalInterface
+public interface WelcomeMessage {
+    void show(String name);
+}
