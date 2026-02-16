58fef210b1f5f49365df0cd79c65a2081b14ed44
Akshat Jain <akshatjain2102004@gmail.com>
2026-02-16 12:31:13 +0530

    [Akshat Jain]: Added DBConnection.java

--- gcr-codebase/DBMSJDBCConcepts/src/main/java/com/dbmsjdbc/jdbc/employeemanagementsystem/DBConnection.java
+++ gcr-codebase/DBMSJDBCConcepts/src/main/java/com/dbmsjdbc/jdbc/employeemanagementsystem/DBConnection.java
@@ -10,7 +10,7 @@ public class DBConnection {
             "jdbc:mysql://localhost:3306/practice_db";
 
     private static final String USER = "root";
-    private static final String PASSWORD = "Ak@939910";
+    private static final String PASSWORD = "*******";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
